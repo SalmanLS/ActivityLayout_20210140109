7f9ce307a6599cd5b55479ebae709c5d6b39967e
SalmanLS <salmanluthfi456@gmail.com>
2023-09-29 19:46:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projectc_1/Projectc_1.kt
@@ -0,0 +1,11 @@
+package com.example.projectc_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Projectc_1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
